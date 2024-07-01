0c03c6d34a23b7d79e2ea9b097f517058ae58c30
hritik11garg <garghritikgarg@gmail.com>
2024-06-30 23:50:30 -0400

    Password Change View

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('',views.index, name = 'index'),
     path('login/', views.user_login, name= 'login'),
     path('logout/', views.user_logout, name= 'logout'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name = 'users/password_change_form.html'), name = 'password_change'),
+    path('password_change/done/',auth_view.PasswordChangeDoneView.as_view(template_name = 'users/password_change_done.html'), name = 'password_change_done'),
 ]
