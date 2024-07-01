04fb9ac78e89434d0025029739df2dc96a5a79ed
hritik11garg <garghritikgarg@gmail.com>
2024-07-01 00:35:40 -0400

    Password Reset : Step 1

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', views.user_logout, name= 'logout'),
     path('password_change/', auth_view.PasswordChangeView.as_view(template_name = 'users/password_change_form.html'), name = 'password_change'),
     path('password_change/done/',auth_view.PasswordChangeDoneView.as_view(template_name = 'users/password_change_done.html'), name = 'password_change_done'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name = 'users/password_reset_form.html'),name = 'password_reset'),
 ]
