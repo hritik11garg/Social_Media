5e20d943c62967ef62d2461e082a1cd9501ce541
hritik11garg <garghritikgarg@gmail.com>
2024-07-01 23:21:57 -0400

    Password Reset Complete View

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('password_reset/', auth_view.PasswordResetView.as_view(template_name = 'users/password_reset_form.html'),name = 'password_reset'),
     path('password_reset/done', auth_view.PasswordResetDoneView.as_view(template_name = 'users/password_reset_done.html'),name = 'password_reset_done'),
     path('reset/<uidb64>/<token>',auth_view.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'),name = 'password_reset_confirm'),
+    path('reset/done/',auth_view.PasswordResetCompleteView.as_view(template_name = 'users/password_reset_complete.html'),name = 'password_reset_complete'),
 ]
