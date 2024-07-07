8c7a07bd8d1910ae6a78dbeb8416be8b457eb5bc
hritik11garg <garghritikgarg@gmail.com>
2024-07-07 00:49:39 -0400

    Edit Profile

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegisterationForm
+from .models import Profile
+from .forms import UserEditForm,ProfileEditForm
 
 def user_login(request):
     if request.method == "POST":
@@ -34,7 +36,21 @@ def register(request):
             new_user = user_form.save(commit= False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            Profile.objects.create(user = new_user)
             return render(request, 'users/register_done.html')
     else:
         user_form = UserRegisterationForm()
     return render(request,'users/register.html',{'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance= request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance= request.user.profile, data=request.POST, files= request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance= request.user)
+        profile_form = ProfileEditForm(instance= request.user.profile)
+    return render(request,'users/edit.html',{'user_form':user_form,'profile_form':profile_form})
