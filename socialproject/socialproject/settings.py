3d3ba2904a1813cffd267bf9628c8df5dc0f50c7
hritik11garg <garghritikgarg@gmail.com>
2024-07-07 00:08:38 -0400

    Extending User Model

--- socialproject/socialproject/settings.py
+++ socialproject/socialproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,5 @@ LOGIN_URL = 'login'
 LOUGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
