aa564c693af2c9498b76171290a74eacf0e6b5c1
hritik11garg <garghritikgarg@gmail.com>
2024-07-06 23:26:55 -0400

    Registering Users

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegisterationForm
 
 def user_login(request):
     if request.method == "POST":
@@ -26,3 +26,15 @@ def user_logout(request):
 @login_required
 def index(request):
     return render(request, 'users/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegisterationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit= False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'users/register_done.html')
+    else:
+        user_form = UserRegisterationForm()
+    return render(request,'users/register.html',{'user_form':user_form})
